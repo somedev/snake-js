55c997741975d4292abf90441ee764bb93b8a38e
Eduard Panasiuk <ed.belarus@gmail.com>
2025-06-05 00:27:44 +0300

    Fix on iPhone

--- sw.js
+++ sw.js
@@ -1,18 +1,24 @@
 const CACHE_NAME = 'snake-game-v1';
 const ASSETS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/game.js',
-    '/manifest.json',
-    '/icons/icon-72x72.png',
-    '/icons/icon-96x96.png',
-    '/icons/icon-128x128.png',
-    '/icons/icon-144x144.png',
-    '/icons/icon-152x152.png',
-    '/icons/icon-192x192.png',
-    '/icons/icon-384x384.png',
-    '/icons/icon-512x512.png'
+    './',
+    './index.html',
+    './style.css',
+    './game.js',
+    './manifest.json',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-167x167.png',
+    './icons/icon-180x180.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png',
+    './icons/splash-640x1136.png',
+    './icons/splash-750x1334.png',
+    './icons/splash-1242x2208.png',
+    './icons/splash-1125x2436.png'
 ];
 
 // Install service worker and cache assets
@@ -48,7 +54,24 @@ self.addEventListener('fetch', (event) => {
                 if (response) {
                     return response;
                 }
-                return fetch(event.request);
+                return fetch(event.request)
+                    .then(response => {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response
+                        const responseToCache = response.clone();
+
+                        // Cache the fetched resource
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    });
             })
     );
 }); 
